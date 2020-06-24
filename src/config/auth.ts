1eb5f3a7094af39878f4f798b1ba47e051841a2c
wilson junior <wilsonmjuniordx@gmail.com>
2020-06-24 17:30:46 -0300

    Added functions to create appointments, users and sessions, and upload user avatar

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4aeee1a5c7837008c69e4893c6adf3f1',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,40 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentsRepositories from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+// Dependecy Inversion (SOLID)
+
+class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(
+      AppointmentsRepositories,
+    );
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentsSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentsSameDate) {
+      throw new Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
